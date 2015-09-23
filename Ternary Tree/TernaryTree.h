c8fc5e4de90a155c285261469c4a47689b66fa92
Shreyaansh Bassi <sbassi@purdue.edu>
2015-09-23 17:47:50 -0400

    Made a .h file

---
+++ Ternary Tree/TernaryTree.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int val;
+    Node *left, *mid, *right;
+    Node(int val) { this->val = val; }
+};
+
+Class Tree {
+    public:
+        
+}
